6caeb2d0ef98d2b81f9ea9f4a050194a10d9bfab
FLM080 <144017590+FLM080@users.noreply.github.com>
2025-01-16 21:11:00 +0100

    route to index page, public folder

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const path = require('path');
 const ejs = require('ejs');
 const port = 3000;
 
+const indexRouter = require('./routes/index');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
