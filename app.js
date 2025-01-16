26dd5f99b966a112067ad43ed21e2b43ef1497a0
FLM080 <144017590+FLM080@users.noreply.github.com>
2025-01-17 00:30:02 +0100

    css linking, showUsers page, small adjustments

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const ejs = require('ejs');
 const port = 3000;
 
 const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-
+app.use(express.static(__dirname + '/public'));
 app.use('/', indexRouter);
+app.use('/', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
