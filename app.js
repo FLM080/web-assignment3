fa2f6d535e5834781e4a918f7bff00f906a344cb
FLM080 <144017590+FLM080@users.noreply.github.com>
2025-01-16 22:22:28 +0100

    navbar

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const indexRouter = require('./routes/index');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
 app.use('/', indexRouter);
 
 app.listen(port, () => {
