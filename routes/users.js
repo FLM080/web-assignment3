a0296a03a8fb1e6905204bf4ccdc5fb2bb2b54f9
Shertone-V1 <mikulajakubko@gmail.com>
2025-01-17 21:04:40 +0100

    View user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/users', (req, res) => {
     res.render('showUsers', { currentRoute: '/users'});
 });
+
+router.get('/viewUsers/:id', (req, res) => {
+    const userId = req.params.id;
+    console.log(userId);
+    res.render('viewUser',{currentRoute: '', userId});
+});
+
 
 module.exports = router;
