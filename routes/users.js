cf850823c619d4af8d3e71e32acab6468ae8b9d7
FLM080 <144017590+FLM080@users.noreply.github.com>
2025-01-17 21:10:44 +0100

    adjustments

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const userController = require('../controllers/userController');
-const { route } = require('.');
-
-router.get('/', userController.getUsers);
+const currentRoute = '/users';
 
 router.get('/users', (req, res) => {
     let users = userController.getUsers();
-    res.render('showUsers', { currentRoute: '/users', users});
+    res.render('showUsers', { currentRoute, users });
 });
 
 module.exports = router;
