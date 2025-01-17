c74113f766730e63dce84d5649ca647f047e7392
Luckkaa <papp.luca.anna@gmail.com>
2025-01-17 21:13:43 +0100

    list users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
+router.get('/', userController.getUsers);
+
+router.get('/users', (req, res) => {
+    let users = userController.getUsers();
+    res.render('showUsers', { currentRoute: '/users', users});
+});
 
 router.get('/users', (req, res) => {
     res.render('showUsers', { currentRoute: '/users'});
@@ -11,6 +18,7 @@ router.get('/viewUsers/:id', (req, res) => {
     console.log(userId);
     res.render('viewUser',{currentRoute: '', userId});
 });
+
 
 
 module.exports = router;
